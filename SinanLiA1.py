676d512faf6df304acf33bf5c76c06ed1ddf1017
sinanli1994 <sinan.li@my.jcu.edu.au>
2018-07-09 17:33:10 +0800

    Optimize code

--- SinanLiA1.py
+++ SinanLiA1.py
@@ -1,7 +1,17 @@
+import operator  # import the operator
+import csv  # import the csv file
+
+
 def main():
+    file_opener = open("list.csv")  # Open the file
+    file_reader = csv.reader(file_opener)  # Read the file
+
+    items_list = sorted(file_reader, key=operator.itemgetter(2))
+    # Import items of the file to the list and sort the list with the priority of items
+
     user_name = input("Please enter your name: ")
     print("Welcome! {}".format(user_name))
-    # Display a welcome message with the name in it
+    # Display a welcome message with the user name
     print("Shopping List 1.0 - by Lindsay Ward\n3 items loaded from items.csv")
 
     menu = """
@@ -20,15 +30,15 @@ def main():
         # Let user enter the choice, convert it to the upper case
         # Call different functions when user enter different choice
         if user_choice == "R":
-            required()
+            required(items_list)
         elif user_choice == "C":
-            completed()
+            completed(items_list)
         elif user_choice == "A":
-            add_new()
+            add_new(items_list)
         elif user_choice == "M":
-            mark()
+            mark(items_list)
         elif user_choice == "Q":
-            quit()
+            quit(items_list)
             break
         else:
             print("Invalid menu choice")
@@ -37,56 +47,44 @@ def main():
     print("Have a nice day :)")
 
 
-def required():  # Function when user want to check the required items
-    import operator
-    # import the operator
-    import csv
-    # import the csv file
-    file_opener = open("list.csv")
-    file_reader = csv.reader(file_opener)
-    # Open the file
+def required(items_list):  # Function when user want to check the required items
     count = 0
     total = 0
 
-    sorted_item = sorted(file_reader, key=operator.itemgetter(2))
-    # sorting the file with the item 2
-    for line in sorted_item:  # using for loop to separate to sorted file
-        if "r" in line[3]:  # When "r" in item 3, then count, calculate and print the required items
+    required_list = sorted(items_list, key=operator.itemgetter(2))
+    # sorting the list with the priority of items
+
+    for item in required_list:  # using for loop to separate to required list
+        if "r" in item[3]:  # When "r" in item 3, then count, calculate and print the required items
             count += 1
-            total += float(line[1])
+            total += float(item[1])
             if count == 1:
                 print("Required items:")
-                print("{}.{:40} $\t{:5}({})".format(count - 1, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count - 1, item[0], item[1], item[2], item[3]))
             else:
-                print("{}.{:40} $\t{:5}({})".format(count - 1, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count - 1, item[0], item[1], item[2], item[3]))
     if count == 0:  # If total count = 0, then means no required item
         print("No required items")
     else:  # Else print total price of required items
         print("Total expected price for {} items: ${}".format(count, total))
 
 
-def completed():  # Function when user want to check the completed items
-    import operator
-    # import the operator
-    import csv
-    # import the csv file
-    file_opener = open("list.csv")
-    file_reader = csv.reader(file_opener)
-    # Open the file
+def completed(items_list):  # Function when user want to check the completed items
     count = 0
     total = 0
 
-    sorted_item = sorted(file_reader, key=operator.itemgetter(2))
-    # sorting the file with the item 2
-    for line in sorted_item:  # using for loop to separate to sorted file
-        if "c" in line[3]:  # When "c" in item 3, then count, calculate and print the completed items
+    completed_list = sorted(items_list, key=operator.itemgetter(2))
+    # sorting the list with the priority of items
+
+    for item in completed_list:  # using for loop to separate to completed list
+        if "c" in item[3]:  # When "c" in item 3, then count, calculate and print the completed items
             count += 1
-            total += float(line[1])
+            total += float(item[1])
             if count == 1:
                 print("Completed items:")
-                print("{}.{:40} $\t{:5}({})".format(count - 1, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count - 1, item[0], item[1], item[2], item[3]))
             else:
-                print("{}.{:40} $\t{:5}({})".format(count - 1, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count - 1, item[0], item[1], item[2], item[3]))
     if count == 0:  # If total count = 0, then means no completed item
         print("No completed items")
     else:
@@ -94,9 +92,7 @@ def completed():  # Function when user want to check the completed items
         # Else print total price of completed items
 
 
-def add_new():  # Function when user want to add new item
-    file = open("list.csv", "a")
-    # Open the file with the correct format
+def add_new(items_list):  # Function when user want to add new item
     item_name = input("Item name:")
     # Let user enter the item name
     while item_name == "":
@@ -125,39 +121,31 @@ def add_new():  # Function when user want to add new item
             else:
                 print("Priority must be 1, 2 or 3")
     # Error check
-    file.write("\n" + item_name + "," + str(price) + "," + str(priority) + "," + "r")
-    # Add the item into the file
-    file.close()
-    # Close the file
+
+    new_item = [item_name, str(price), str(priority), "r"]  # Create a list with new item
+    items_list.append(new_item)  # Appending the new list to the item list
+
     print("{}, ${} (priority {}) added to shopping list".format(item_name, price, priority))
     # Display the added item data
 
 
-def mark():  # Function when user want to mark the required item to completed item
-    import operator
-    # import the operator
-    import csv
-    # import the csv file
-    file_opener = open("list.csv")
-    file_reader = csv.reader(file_opener)
-    file_writer = open("list.csv", "a")
-    # Open the file
+def mark(items_list):  # Function when user want to mark the required item to completed item
     count = -1
     count1 = -1
     total = 0
 
-    sorted_item = sorted(file_reader, key=operator.itemgetter(2))
-    # sorting the file with the item 2
+    required_list = sorted(items_list, key=operator.itemgetter(2))
+    # sorting the list with the priority of items
 
-    for line in sorted_item:  # using for loop to separate to sorted file
-        if "r" in line[3]:  # When "r" in item 3, then count, calculate and print the required items
+    for item in required_list:  # using for loop to separate to required list
+        if "r" in item[3]:  # When "r" in item 3, then count, calculate and print the required items
             count += 1
-            total += float(line[1])
+            total += float(item[1])
             if count == 0:
                 print("Required items:")
-                print("{}.{:40} $\t{:5}({})".format(count, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count, item[0], item[1], item[2], item[3]))
             else:
-                print("{}.{:40} $\t{:5}({})".format(count, line[0], line[1], line[2], line[3]))
+                print("{}.{:40} $\t{:5}({})".format(count, item[0], item[1], item[2], item[3]))
     if count == -1:  # If total count = 0, then means no required item
         print("No required items")
     else:  # Else print total price of required items
@@ -170,48 +158,41 @@ def mark():  # Function when user want to mark the required item to completed it
                 print("Invalid input; enter a number")
             else:
                 if count >= user_input >= 0:
-                    for line2 in sorted_item:  # Find the number and rewrite the items in the file and print the completed item
-                        if "r" in line2[3]:
+                    for item2 in required_list:  # Find and mark the item in the list, then print the completed item
+                        if "r" in item2[3]:
                             count1 += 1
                             if count1 == user_input:
-                                file_writer2 = open("list.csv", "w")
-                                file_writer2.writelines(line2[0] + "," + line2[1] + "," + line2[2] + "," + "c")
-                                print("{} marked as completed".format(line2[0]))
-                                file_writer2.close()
-                            else:
-                                file_writer.writelines("\n" + line2[0] + "," + line2[1] + "," + line2[2] + "," + line2[3])
-                        else:
-                            file_writer.writelines("\n" + line2[0] + "," + line2[1] + "," + line2[2] + "," + line2[3])
+                                item2[3] = "c"  # Using "c" instead of "r" in the list when user chooses the item
+                                print("{} marked as completed".format(item2[0]))
+                                # Display the marked item
                     break
                 else:
                     print("Invalid item number")
                     # Error check
-    file_writer.close()
-    # Close the file
 
 
-def quit():  # Function when user want to quit
-    file_reader = open("list.csv", "r")
+def quit(items_list):  # Function when user want to quit
     file_writer = open("list.csv", "a")
     # Open the file with the correct format
     count = 0
 
-    for line in file_reader:  # Find the completed items the delete the required items
-        char = line.split(",")
-        if "c" in char[3]:
+    for item in items_list:  # Find the completed items and save to the csv file
+        if "c" in item[3]:
             count += 1
             if count == 1:
                 file_writer2 = open("list.csv", "w")
-                file_writer2.writelines(line.strip())
+                file_writer2.writelines(item[0] + "," + item[1] + "," + item[2] + "," + "c")
                 file_writer2.close()
             else:
-                file_writer.writelines("\n" + line.strip())
+                file_writer.writelines("\n" + item[0] + "," + item[1] + "," + item[2] + "," + "c")
         else:
-            file_writer.writelines(line.replace(line, "").strip())
+            file_writer.writelines("")
+    if count == 0:
+        file_writer2 = open("list.csv", "w")
+        file_writer2.close()
 
     print("{} items saved to items.csv".format(count))
     # Display how many items which add to the file
-    file_reader.close()
     file_writer.close()
     # Close the file
 
